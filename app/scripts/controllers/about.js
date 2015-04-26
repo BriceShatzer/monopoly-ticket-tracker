28613a7f44e9758109c1a82a5746640466cee62a
Brice Shatzer <brice.shatzer@gmail.com>
2015-04-26 18:27:32 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monopolyTicketTrackerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the monopolyTicketTrackerApp
+ */
+angular.module('monopolyTicketTrackerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monopolyTicketTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the monopolyTicketTrackerApp
+ */
+angular.module('monopolyTicketTrackerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
