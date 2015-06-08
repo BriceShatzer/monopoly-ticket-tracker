865414edc58b7d3c720f86f490ddae62a713aaf6
Brice Shatzer <brice.shatzer@gmail.com>
2015-06-07 22:34:43 -0500

    local storage service

---
+++ app/scripts/services/localStorage.js
@@ -0,0 +1,15 @@
+'use strict';
+
+ /**
+ * @ngdoc function
+ * @name monopolyTicketTrackerApp.service:localStorage
+ * @description
+ * # localStorage
+ * Service of the monopolyTicketTrackerApp
+ */
+angular.module('monopolyTicketTrackerApp')
+    .factory('localStorage', ['$window', function(win) {
+    	return function() {
+    		win.alert('triggered alert');
+    	};
+    }]);
